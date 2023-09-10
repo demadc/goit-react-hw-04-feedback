3dadc2e93b526e4977c20890adeb42bc256861ba
demadc <40273971+demadc@users.noreply.github.com>
2023-09-10 17:19:16 +0000

    Deploying to gh-pages from @ demadc/goit-react-hw-04-feedback@a884f18b449bcdb30a823cffcb94626b34f51f1d 🚀

--- src/components/NotificationMessage/NotificationMessage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Message } from './NotificationMessage.styled';
-
-export const NotificationMessage = ({ message }) => {
-  return <Message>{message}</Message>;
-};
